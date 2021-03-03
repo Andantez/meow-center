a5786d985fa07e0075bda11789e91d41df65c36e
Andantez <kaloyanatanasov83@gmail.com>
2021-03-03 20:39:25 +0000

    ADD: environment variable for the api key

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+const handler = async (req, res) => {
+  const response = await fetch('https://api.thecatapi.com/v1/favourites', {
+    headers: {
+      'x-api-key': process.env.X_API_KEY,
+    },
+  });
+  const data = await response.json();
+  res.status(200).json(data)
 }
+
+export default handler;
