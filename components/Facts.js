dd41a07bec7a6327d38c15aff0aee162a29a95c0
Andantez <kaloyanatanasov83@gmail.com>
2021-05-14 20:59:34 +0100

    Add: html structure of the facts section.

--- components/Facts.js
+++ components/Facts.js
@@ -1,5 +1,44 @@
+import styled from 'styled-components';
+import Image from 'next/image';
+import factsTempData from '../data/tempFacts';
+
 const Facts = () => {
-  return <div>Facts</div>;
+  const { data } = factsTempData;
+  return (
+    <StyledSection>
+      <h2>interesting facts about cats</h2>
+      <div>
+        {data.map((singleFact) => {
+          {/* todo extract it to single component */}
+          return (
+            <article>
+              <p>{singleFact.fact}</p>
+            </article>
+          );
+        })}
+      </div>
+      <Image
+        src="/images/homepage-cat1.jpg"
+        width="300"
+        height="200"
+        alt="ginger cat cleaning itself"
+      />
+      <Image
+        src="/images/homepage-cat2.jpg"
+        width="300"
+        height="200"
+        alt="cat lying on its back"
+      />
+      <Image
+        src="/images/homepage-cat3.jpg"
+        width="200"
+        height="300"
+        alt="cat lying on its back"
+      />
+      <button>get more facts</button>
+    </StyledSection>
+  );
 };
 
+const StyledSection = styled.section``;
 export default Facts;
