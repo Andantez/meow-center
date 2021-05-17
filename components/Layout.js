0b6df2fabcdf588f648135a7401bc24ad1d5297e
Andantez <kaloyanatanasov83@gmail.com>
2021-05-17 15:19:04 +0100

    Add: footer and styling to it. Change: color of pseudo element color on hover of header links.

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Header from '../components/Header';
-
+import Footer from '../components/Footer';
 const Layout = ({ children }) => {
   return (
     <>
@@ -13,6 +13,7 @@ const Layout = ({ children }) => {
       </Head>
       <Header />
       {children}
+      <Footer />
     </>
   );
 };
