3b0890e2a2fdb6517578167ad7b12384304ac1b4
Andantez <kaloyanatanasov83@gmail.com>
2021-05-24 23:03:56 +0100

    Feat:Filters Component. Add: file with temperament list and function to get all of the unique breed origins.

--- components/Filters.js
+++ components/Filters.js
@@ -1,7 +1,52 @@
+import styled from 'styled-components';
+import FiltersSearch from './FiltersSearch';
+import temperamentList from '../data/temperamentList';
+import { getUnuqueValues } from '../utils/helpers';
 
-
+import tempData from '../data/tempData';
 const Filters = () => {
-  return <div>Filters Component</div>;
+  const data = tempData; // to be change to get the data from the context later.
+
+  const breedOrigins = getUnuqueValues(data, 'origin');
+  return (
+    <StyledDiv>
+      <form>
+        <div className="form-control">
+          <FiltersSearch />
+        </div>
+        <div className="form-control">
+          {temperamentList.map((temperament) => {
+            return (
+              <button type="button" className="btn">
+                {temperament}
+              </button>
+            );
+          })}
+        </div>
+        <div className="form-control">
+          <label htmlFor="origins">Origin</label>
+          <select name="origin" id="origins">
+            {breedOrigins.map((origin) => {
+              return (
+                <option key={origin} value={origin}>
+                  {origin}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      </form>
+    </StyledDiv>
+  );
 };
 
+const StyledDiv = styled.div`
+  .form-control {
+    display: grid;
+    width: 200px; /* temporary */
+  }
+  .form-control:not(:last-child) {
+    border-bottom: 2px solid var(--clr-black);
+  }
+`;
 export default Filters;
