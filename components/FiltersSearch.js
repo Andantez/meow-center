446f6275c02b6f9de508228df55069909ab9bc01
Andantez <kaloyanatanasov83@gmail.com>
2021-05-31 19:03:58 +0100

    Change: filters search background, add border and changed it to default outline focus style.

--- components/FiltersSearch.js
+++ components/FiltersSearch.js
@@ -8,20 +8,23 @@ const StyledInput = styled.input`
   font-family: var(--ff-paragraph);
   font-size: 0.8125rem;
   border: transparent;
-  background-color: #e4e6eb;
+  background-color: transparent;
   border-radius: 0.5em;
   padding: 0.5em;
   width: fit-content;
   transition: background-color 350ms ease;
-  &:focus {
-    outline: 2px solid var(--clr-black);
-  }
+  border: 1px solid var(--clr-primary-100);
+  
   ::placeholder {
     color: var(--clr-primary-500);
   }
 
   :hover {
-    background-color: #d6d9e1;
+    background-color: #e4e6eb;
+  }
+
+  @media (min-width: 1024px) {
+    width: 100%;
   }
 `;
 export default FiltersSearch;
