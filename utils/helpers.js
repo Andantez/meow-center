1707e8fb25e3f6d8e31aaa6b57a8cde0f90d7050
Andantez <kaloyanatanasov83@gmail.com>
2021-06-03 00:26:51 +0100

    helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -6,3 +6,37 @@ export const getUniqueValues = (data, type) => {
 
   return ['All', ...new Set(origins)];
 };
+
+export const getCharacteristics = (data) => {
+  const {
+    adaptability,
+    affection_level,
+    child_friendly,
+    energy_level,
+    grooming,
+    health_issues,
+    social_needs,
+    intelligence,
+    stranger_friendly,
+    hypoallergenic,
+    life_span,
+    weight: { metric: weight },
+  } = data;
+  const characteristics = {
+    adaptability,
+    affection_level,
+    child_friendly,
+    energy_level,
+    grooming,
+    health_issues,
+    social_needs,
+    intelligence,
+    stranger_friendly,
+    life_span,
+    hypoallergenic,
+    weight,
+  };
+  return Object.entries(characteristics).map((entry) => {
+    return { characteristic: entry[0].split('_').join(' '), value: entry[1] };
+  });
+};
