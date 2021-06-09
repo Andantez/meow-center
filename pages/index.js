a9e4d5740db58b4c63cf9fb25b2218d6ed43ae3b
Andantez <kaloyanatanasov83@gmail.com>
2021-06-09 23:12:21 +0100

    Update: NextJs version to the latest one.

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,9 @@ import Head from 'next/head';
 import Hero from '../components/Hero';
 import MostPopular from '../components/MostPopular';
 import Facts from '../components/Facts';
-import { connectToDatabase } from '../utils/mongodb';
+// import { connectToDatabase } from '../utils/mongodb';
 
-export default function Home({ isConnected }) {
-  console.log(isConnected)
+export default function Home() {
   return (
     <>
       <Head>
@@ -18,11 +17,21 @@ export default function Home({ isConnected }) {
   );
 }
 
-export const getStaticProps = async () => {
-  const { client } = await connectToDatabase();
-  const isConnected = await client.isConnected();
+// export const getStaticProps = async () => {
+//   const { db } = await connectToDatabase();
+//   const insertedResponse = await db.collection('mostpopular').findOneAndUpdate(
+//     { breedId: 'abys' },
+//     { $inc: { score: 1 } },
+//     {
+//       returnOriginal: false,
+//       projection: {
+//         name: 1,
+//         score: 1,
+//       },
+//     }
+//   );
 
-  return {
-    props: { isConnected },
-  };
-};
+//   return {
+//     props: { insertedResponse: JSON.parse(JSON.stringify(insertedResponse)) },
+//   };
+// };
