27914b6c02c7d96c3aa1520e2288c16b76f441fc
Andantez <kaloyanatanasov83@gmail.com>
2021-06-11 00:08:56 +0100

    Add: gridView and listView action and functions. Change: grid,list button based on condtion.

--- components/ListButton.js
+++ components/ListButton.js
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 import { FaThList } from 'react-icons/fa';
-
+import { useFiltersContext } from '../context/filters_context';
 const ListButton = () => {
+  const { setListView, gridView } = useFiltersContext();
   return (
-    <StyledButton type="button">
+    <StyledButton type="button" onClick={setListView} gridView={gridView}>
       <FaThList />
     </StyledButton>
   );
@@ -11,13 +12,14 @@ const ListButton = () => {
 
 const StyledButton = styled.button`
   width: fit-content;
-  color: #b5b5b5;
+  color: ${(props) => (props.gridView ? '#b5b5b5' : 'var(--clr-black)')};
   display: flex;
   align-items: center;
   border: transparent;
   background-color: transparent;
   cursor: pointer;
   transition: transform 250ms ease;
+  transform: ${(props) => (props.gridView ? 'scale(1)' : 'scale(1.1)')};
   svg {
     font-size: 1.5rem;
   }
