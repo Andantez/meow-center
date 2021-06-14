fda512ccc43dbb259638e2f1403e0b6cafb53a0a
Andantez <kaloyanatanasov83@gmail.com>
2021-06-14 22:22:39 +0100

    Add: search functionality.

--- context/home_context.js
+++ context/home_context.js
@@ -1,20 +1,55 @@
 import { createContext, useContext, useState } from 'react';
+import useSWR from 'swr';
 
 const HomeContext = createContext();
 
 const HomeProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [query, setQuery] = useState('');
+  const [itsOnFocus, setItsOnFocus] = useState(false);
+  const { data: searchResults } = useSWR(
+    () => query ? `https://api.thecatapi.com/v1/breeds/search?q=${query}` : null
+  );
   
+  const isLoadingData = !searchResults && query.length > 0;
+  const noResultsFound =
+    searchResults?.length === 0 && query.length > 0 && itsOnFocus;
+
+  // console.log("is loading", isLoading, "nqma resultati", noResultsFound);
+
   const openSidebar = () => {
     setIsSidebarOpen(true);
-  }
+  };
 
   const closeSidebar = () => {
-    setIsSidebarOpen(false)
-  }
-  
+    setIsSidebarOpen(false);
+  };
+
+  const setUserQuery = (userQuery) => {
+    setQuery(userQuery);
+  };
+  const setFocus = () => {
+    setItsOnFocus(true);
+  };
+  const setBlur = () => {
+    setItsOnFocus(false);
+  };
   return (
-    <HomeContext.Provider value={{openSidebar, closeSidebar, isSidebarOpen}}>
+    <HomeContext.Provider
+      value={{
+        openSidebar,
+        closeSidebar,
+        isSidebarOpen,
+        setUserQuery,
+        searchResults,
+        query,
+        setFocus,
+        setBlur,
+        itsOnFocus,
+        isLoadingData,
+        noResultsFound,
+      }}
+    >
       {children}
     </HomeContext.Provider>
   );
