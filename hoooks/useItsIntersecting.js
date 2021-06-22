d460c02cb961c637d19eec3484b13ef6a396780d
Andantez <kaloyanatanasov83@gmail.com>
2021-06-22 23:34:09 +0100

    Add: useItsIntersectin hook to detect when target is visible. Infinite scrolling is working.

---
+++ hoooks/useItsIntersecting.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+const useItsIntersecting = (ref) => {
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  const options = {
+    root: ref.current,
+    rootMargin: '50%',
+    threshold: 0,
+  };
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => setIntersecting(entry.isIntersecting),
+      options
+    );
+    console.log(ref);
+    observer.observe(ref.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return isIntersecting;
+};
+
+export default useItsIntersecting;
