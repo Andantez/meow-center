f59fcb5d278d1dab0ab999f256f28380ff8c40d4
Andantez <kaloyanatanasov83@gmail.com>
2021-07-19 23:11:26 +0100

    Add: state to control legend keys. Add: styling to legen div.

--- components/Charts/BarChart.js
+++ components/Charts/BarChart.js
@@ -16,6 +16,11 @@ const BarChart = ({ data }) => {
   const [layout, setLayout] = useState('vertical');
   const [selectedBreeds, setSelectedBreeds] = useState([]);
   const [isSelected, setIsSelected] = useState(false);
+  const [legendKeys, setLegendKeys] = useState([
+    'min lifespan',
+    'max lifespan',
+    'avg weight(kg)',
+  ]);
   const layoutIsVertical = layout === 'vertical';
   const handleActivePage = (index) => {
     setActivePage(index);
@@ -38,7 +43,17 @@ const BarChart = ({ data }) => {
       return tempState;
     });
   };
+  const handleOnClick = (e) => {
+    const findKey = legendKeys.find((key) => key === e.target.name);
 
+    if (findKey) {
+      const filteredKeys = legendKeys.filter((key) => key !== e.target.name);
+      setLegendKeys(filteredKeys);
+    } else {
+      const filteredKeys = [...legendKeys, e.target.name].sort((a, b) => b.localeCompare(a));
+      setLegendKeys(filteredKeys);
+    }
+  };
   useEffect(() => {
     if (selectedBreeds.length > 0) {
       setIsSelected(true);
@@ -79,77 +94,99 @@ const BarChart = ({ data }) => {
           </button>
         </StyledForm>
       </div>
-      <ResponsiveBar
-        data={isSelected ? selectedBreeds : data[activePage]}
-        keys={['min lifespan', 'max lifespan', 'avg weight(kg)']}
-        indexBy="breed name"
-        margin={{
-          top: 10,
-          right: 30,
-          bottom: 50,
-          left: layoutIsVertical ? 30 : 120,
-        }}
-        padding={0.1}
-        innerPadding={2}
-        groupMode="grouped"
-        valueScale={{ type: 'linear' }}
-        indexScale={{ type: 'band', round: true }}
-        colors={getColor}
-        layout={layout}
-        borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
-        axisTop={null}
-        axisRight={null}
-        // axisBottom={{
-        //   tickSize: 5,
-        //   tickPadding: 5,
-        //   tickRotation: 0,
-        //   legend: 'Breed Name',
-        //   legendPosition: 'middle',
-        //   legendOffset: 42,
-        // }}
-        theme={{
-          axis: {
-            ticks: {
-              text: {
-                fontSize: 11,
-                fill: '#343446',
-                fontFamily: 'Bitter',
+      <div className="bar-chart">
+        <ResponsiveBar
+          data={isSelected ? selectedBreeds : data[activePage]}
+          keys={legendKeys}
+          indexBy="breed name"
+          margin={{
+            top: 10,
+            right: 30,
+            bottom: 50,
+            left: layoutIsVertical ? 30 : 120,
+          }}
+          padding={0.1}
+          innerPadding={2}
+          groupMode="grouped"
+          valueScale={{ type: 'linear' }}
+          indexScale={{ type: 'band', round: true }}
+          colors={getColor}
+          layout={layout}
+          borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+          axisTop={null}
+          axisRight={null}
+          // axisBottom={{
+          //   tickSize: 5,
+          //   tickPadding: 5,
+          //   tickRotation: 0,
+          //   legend: 'Breed Name',
+          //   legendPosition: 'middle',
+          //   legendOffset: 42,
+          // }}
+          theme={{
+            axis: {
+              ticks: {
+                text: {
+                  fontSize: 11,
+                  fill: '#343446',
+                  fontFamily: 'Bitter',
+                },
               },
             },
-          },
-        }}
-        labelSkipWidth={12}
-        labelSkipHeight={12}
-        labelTextColor="#343446"
-        // legends={[
-        //   {
-        //     dataFrom: 'keys',
-        //     anchor: 'bottom-right',
-        //     direction: 'column',
-        //     justify: false,
-        //     translateX: 120,
-        //     translateY: 0,
-        //     itemsSpacing: 2,
-        //     itemWidth: 100,
-        //     itemHeight: 20,
-        //     itemDirection: 'left-to-right',
-        //     itemOpacity: 0.85,
-        //     itemTextColor: '#343446',
-        //     symbolSize: 15,
-        //     effects: [
-        //       {
-        //         on: 'hover',
-        //         style: {
-        //           itemOpacity: 1,
-        //         },
-        //       },
-        //     ],
-        //   },
-        // ]}
-        animate={true}
-        motionStiffness={90}
-        motionDamping={15}
-      />
+          }}
+          labelSkipWidth={12}
+          labelSkipHeight={12}
+          labelTextColor="#343446"
+          // legends={[
+          //   {
+          //     dataFrom: 'keys',
+          //     anchor: 'bottom-right',
+          //     direction: 'column',
+          //     justify: false,
+          //     translateX: 120,
+          //     translateY: 0,
+          //     itemsSpacing: 2,
+          //     itemWidth: 100,
+          //     itemHeight: 20,
+          //     itemDirection: 'left-to-right',
+          //     itemOpacity: 0.85,
+          //     itemTextColor: '#343446',
+          //     symbolSize: 15,
+          //     effects: [
+          //       {
+          //         on: 'hover',
+          //         style: {
+          //           itemOpacity: 1,
+          //         },
+          //       },
+          //     ],
+          //   },
+          // ]}
+          animate={true}
+          motionStiffness={90}
+          motionDamping={15}
+        />
+        <div className="legend">
+          <div>
+            <span className="symbol"></span>
+            <button type="button" name="min lifespan" onClick={handleOnClick}>
+              min lifespan
+            </button>
+          </div>
+          <div>
+            <span className="symbol"></span>
+            <button type="button" name="max lifespan" onClick={handleOnClick}>
+              max lifespan
+            </button>
+          </div>
+          <div>
+            <span className="symbol"></span>
+            <button type="button" name="avg weight(kg)" onClick={handleOnClick}>
+              avg weight(kg)
+            </button>
+          </div>
+        </div>
+      </div>
       <Pagination
         data={data}
         handleNext={handleNext}
@@ -192,14 +229,11 @@ const StyledDiv = styled.div`
   .select-wrapper {
     display: flex;
     justify-content: space-between;
-    /* flex-direction: column; */
-    /* margin-top: 1em; */
   }
 
   .breeds-select {
     display: flex;
     align-items: center;
-    /* margin-top: 0.5em; */
 
     label {
       font-weight: var(--fw-bold);
@@ -216,6 +250,49 @@ const StyledDiv = styled.div`
       font-size: 1.25rem;
       color: var(--clr-primary-500);
     }
+  }
+  .legend {
+    display: grid;
+    gap: 0.5em;
+    place-self: flex-end;
+    margin-bottom: 3.125em;
+    width: max-content;
+  }
+  .legend > div {
+    display: flex;
+    align-items: center;
+    font-size: 0.8125rem;
+  }
+  .symbol {
+    display: inline-block;
+    width: 1.5em;
+    height: 1.5em;
+  }
+  .legend div:nth-child(1) span {
+    background-color: #ffc09f;
+  }
+  .legend div:nth-child(2) span {
+    background-color: #ffee93;
+  }
+  .legend div:nth-child(3) span {
+    background-color: #a0ced9;
+  }
+  .legend button {
+    margin-left: 0.5em;
+    font-size: 0.8125rem;
+    color: var(--clr-primary-500);
+    background: transparent;
+    border: transparent;
+    cursor: pointer;
+    &:hover {
+      color: var(--clr-grey);
+    }
+  }
+
+  .bar-chart {
+    display: grid;
+    grid-template-rows: 500px;
+    grid-template-columns: 1fr auto;
   }
   @media (min-width: 768px) {
     .multi-select,
