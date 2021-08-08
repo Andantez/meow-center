3325ca03aaab0359ce2901826d982350b15a8f9d
Andantez <kaloyanatanasov83@gmail.com>
2021-08-08 22:34:21 +0100

    Change: hover controlled by framer. Add: AnimatePresence to app for page transitions.

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,12 @@ import Layout from '../components/Layout';
 import HomeProvider from '../context/home_context';
 import FiltersContextProvider from '../context/filters_context';
 import { SWRConfig } from 'swr';
-
 import '../styles/globals.css';
+import { AnimatePresence } from 'framer-motion';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <HomeProvider>
       <FiltersContextProvider>
@@ -20,7 +22,9 @@ function MyApp({ Component, pageProps }) {
           }}
         >
           <Layout>
-            <Component {...pageProps} />
+            <AnimatePresence exitBeforeEnter>
+              <Component {...pageProps} key={router.route} />
+            </AnimatePresence>
           </Layout>
         </SWRConfig>
       </FiltersContextProvider>
