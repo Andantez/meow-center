48718e6e2a81279c420fa5b603bb6ee25554e336
Andantez <kaloyanatanasov83@gmail.com>
2021-11-09 19:03:44 +0000

    Cloudinary automatically serve the hero images format.

--- components/MyImage.js
+++ components/MyImage.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 const myLoader = ({ src, width, quality }) => {
-  return `https://res.cloudinary.com/andantez/image/upload/w_${width},q_${
+  return `https://res.cloudinary.com/andantez/image/upload/f_auto,w_${width},q_${
     quality || 75
   }/${src}`;
 };
