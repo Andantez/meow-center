9e22e811f4a3ff6f0d8a716ccbc6711a303a7cb1
Andantez <kaloyanatanasov83@gmail.com>
2021-12-12 21:40:23 +0000

    Add:forgotten password api route gerate random identifier and hash it with expiration time to db if user exists.

--- pages/api/auth/forgottenpassword.js
+++ pages/api/auth/forgottenpassword.js
@@ -1,10 +1,49 @@
+import clientPromise from '../../../utils/mongodb';
+
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const { email } = req.body;
-    res.status(200).json({
-      success: true,
-      message: `We've sent an email to ${email} with a link to create new password.`,
-    });
+    try {
+      const { email } = req.body;
+      const crypto = await import('crypto');
+      const client = await clientPromise;
+      const db = await client.db();
+
+      const existingUser = await db.collection('users').findOne({ email });
+
+      if (!existingUser) {
+        return res.status(200).json({
+          success: true,
+          message: `We've sent an email to ${email} with a link to create new password.`,
+        });
+      }
+
+      const resetToken = crypto.randomBytes(32).toString('hex');
+
+      const hashedPasswordResetToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+      const tokenExpirationTime = new Date(Date.now() + 10 * 60 * 1000);
+
+      const updatedUser = await db
+        .collection('users')
+        .updateOne(
+          { email },
+          { $set: { hashedPasswordResetToken, tokenExpirationTime } }
+        );
+
+      // not completed yet
+
+      res.status(200).json({
+        success: true,
+        message: `We've sent an email to ${email} with a link to create new password.`,
+      });
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json({ success: false, message: 'Internal server error', error });
+    }
   } else {
     res
       .status(405)
