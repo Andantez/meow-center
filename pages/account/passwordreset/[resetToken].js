f556ad78e9bf2d4d3a3fc90f407d243419df02a6
Andantez <kaloyanatanasov83@gmail.com>
2021-12-14 21:10:41 +0000

    add: nodemailer and sending email through sendgrid.

---
+++ pages/account/passwordreset/[resetToken].js
@@ -0,0 +1,6 @@
+
+const ResetToken = () => {
+  return <h2>reset token page </h2>;
+};
+
+export default ResetToken;
---
+++ pages/account/passwordreset/index.js
@@ -0,0 +1,5 @@
+const PasswordReset = () => {
+  return <h1>Password Reset Page</h1>;
+};
+
+export default PasswordReset;
