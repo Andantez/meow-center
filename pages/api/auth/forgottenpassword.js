0612206ad5968ec6121df7ef9cd453b41e309ed7
Andantez <kaloyanatanasov83@gmail.com>
2021-12-18 21:46:51 +0000

    Change: email text content.

--- pages/api/auth/forgottenpassword.js
+++ pages/api/auth/forgottenpassword.js
@@ -38,9 +38,9 @@ export default async function handler(req, res) {
         const url = `https://meow-portal.vercel.app/account/passwordreset/${resetToken}`;
         const html = `<span style="opacity: 0">${Date.now()} </span>
           <p>Hi ${existingUser.name},</p>
-          <p>There was a request to change your password!</p>
+          <p>There was a request to change your password for the Meow Portal account associated with this email.</p>
+          <p> You  can reset your password clicking the link: <a href="${url}" clickTracking=off style="text-decoration: underline;font-weight: bold;">Reset your password</a></p>
           <p>This password reset link is only valid for <span style="font-weight: bold">10 minutes</span> from receiving this email.
-          <p> Otherwise, please click the link to change your password: <a href="${url}" clickTracking=off style="text-decoration: underline;font-weight: bold;">Reset your password</a></p>
           <small>If you did not make this request then please ignore this email. Only a person with access to your email can reset your password</small>
           <span style="opacity: 0">${Date.now()} </span>
           `;
@@ -48,7 +48,7 @@ export default async function handler(req, res) {
         const info = await sendEmail({
           to: existingUser.email,
           subject: 'Reset your Meow Portal password',
-          text: `Hi ${existingUser.name} There was a request to change your password! If you did not make this request then please ignore this email. Otherwise, please click this link to change your password: <a href="${url}">Reset your password</a>`,
+          text: `Hi ${existingUser.name} There was a request to change your password for the Meow Portal account associated with this email. You can reset your password by clicking the link: <a href="${url}"> . This password reset link is only valid for 10 minutes from receiving this email. If you did not make this request then please ignore this email. Only a person with access to your email can reset your password`,
           html: html,
           // span tags are to ensure gmail is not going to trimm the content.
         });
