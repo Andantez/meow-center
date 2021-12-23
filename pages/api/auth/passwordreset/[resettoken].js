90f44634b64105da1f8da1ba419790766c918087
Andantez <kaloyanatanasov83@gmail.com>
2021-12-23 21:44:38 +0000

    Add: validate password to meet certain condition.

--- pages/api/auth/passwordreset/[resettoken].js
+++ pages/api/auth/passwordreset/[resettoken].js
@@ -1,8 +1,21 @@
 import clientPromise from '../../../../utils/mongodb';
 import bcrypt from 'bcrypt';
+import { validatePassword } from '../../../../utils/helpers';
+
 export default async function handler(req, res) {
   if (req.method === 'PUT') {
     try {
+      const password = req.body.password;
+      const isValidPassword = validatePassword(password);
+      if (!isValidPassword) {
+        res.status(422).json({
+          success: false,
+          error: 'Invalid Password Criteria',
+          message:
+            'Your password must be at least 6 characters long, contain at least one number and have a combination of uppercase and lowercase letters.',
+        });
+        return;
+      }
       const crypto = await import('crypto');
       const client = await clientPromise;
       const db = await client.db();
@@ -19,12 +32,15 @@ export default async function handler(req, res) {
         }
       );
       if (!existingUser) {
-        res
-          .status(401)
-          .json({ success: false, message: 'Your reset link has expired. Please re-enter your email and we will send you new link.' });
-          return;
+        res.status(401).json({
+          success: false,
+          error: 'Token Expired',
+          message:
+            'Your reset link has expired. Please re-enter your email and we will send you new link.',
+        });
+        return;
       }
-      
+
       const hashedPassword = await bcrypt.hash(req.body.password, 12);
       const updatedUser = await db.collection('users').updateOne(
         {
@@ -40,11 +56,21 @@ export default async function handler(req, res) {
       );
       res
         .status(200)
-        .json({ success: true, message: 'Password successfully changed.' });
+        .json({
+          success: true,
+          error: null,
+          message: 'Password successfully changed.',
+        });
     } catch (error) {
       console.log(error);
     }
   } else {
-    res.status(405).json({succes: true, message: `${req.method} method is not allowed`})
+    res
+      .status(405)
+      .json({
+        succes: true,
+        error: 'Method not allowed',
+        message: `${req.method} method is not allowed`,
+      });
   }
 }
