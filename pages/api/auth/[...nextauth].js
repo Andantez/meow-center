c1be33104e97f0bd373a41aebdb973711737c484
Andantez <kaloyanatanasov83@gmail.com>
2022-01-06 23:45:27 +0000

    Add: session update when user change their info.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,94 +6,96 @@ import clientPromise from '../../../utils/mongodb';
 import bcrypt from 'bcrypt';
 import { ObjectId } from 'mongodb';
 
-export default NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-      authorization: {
-        params: {
-          prompt: 'consent',
-          access_type: 'offline',
-          response_type: 'code',
+export default async function auth(req, res) {
+  return NextAuth(req, res, {
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET,
+        authorization: {
+          params: {
+            prompt: 'consent',
+            access_type: 'offline',
+            response_type: 'code',
+          },
         },
-      },
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-      authorization: {
-        params: {
-          scope: 'email,public_profile',
-          // auth_type: 'reauthenticate',
+      }),
+      FacebookProvider({
+        clientId: process.env.FACEBOOK_CLIENT_ID,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+        authorization: {
+          params: {
+            scope: 'email,public_profile',
+            // auth_type: 'reauthenticate',
+          },
         },
+      }),
+      CredentialsProvider({
+        name: 'Credentials',
+        async authorize(credentials, req) {
+          try {
+            const client = await clientPromise;
+            const db = client.db();
+            const existingUser = await db
+              .collection('users')
+              .findOne({ email: credentials.email });
+
+            if (existingUser) {
+              const isAuthenticated = await bcrypt.compare(
+                credentials.password,
+                existingUser.password
+              );
+
+              if (isAuthenticated) {
+                const { name, email } = existingUser;
+
+                return { name, email, id: existingUser._id };
+              }
+            }
+          } catch (error) {
+            return null;
+          }
+          return null;
+        },
+      }),
+    ],
+    session: {
+      strategy: 'jwt',
+    },
+    callbacks: {
+      async session({ session, token }) {
+        session.user = token.user;
+        return session;
       },
-    }),
-    CredentialsProvider({
-      name: 'Credentials',
-      async authorize(credentials, req) {
-        try {
+      async jwt({ token, user, account }) {
+        if (req.url === '/api/auth/session?update') {
           const client = await clientPromise;
-          const db = client.db();
+          const db = await client.db();
+          const u_ObjectId = new ObjectId(token.user.id);
           const existingUser = await db
             .collection('users')
-            .findOne({ email: credentials.email });
-
-          if (existingUser) {
-            const isAuthenticated = await bcrypt.compare(
-              credentials.password,
-              existingUser.password
-            );
-
-            if (isAuthenticated) {
-              const { name, email } = existingUser;
-
-              return { name, email, id: existingUser._id };
-            }
-          }
-        } catch (error) {
-          return null;
+            .findOne({ _id: u_ObjectId });
+          const user = {
+            name: existingUser.name,
+            email: existingUser.email,
+            id: existingUser._id.toString(),
+            provider: token.user.provider,
+          };
+          // TODO finish it.
+          token.user = user;
+          return token;
         }
-        return null;
-      },
-    }),
-  ],
-  session: {
-    strategy: 'jwt',
-  },
-  callbacks: {
-    async session({ session, token }) {
-      if (token.user.provider === 'credentials') {
-        const client = await clientPromise;
-        const db = await client.db();
-        const u_ObjectId = new ObjectId(token.user.id);
-        const existingUser = await db
-          .collection('users')
-          .findOne({ _id: u_ObjectId });
-        const user = {
-          name: existingUser.name,
-          email: existingUser.email,
-          id: existingUser._id.toString(),
-          provider: token.user.provider,
-        };
-        // TODO finish it.
-        session.user = user;
-        return session;
-      }
 
-      session.user = token.user;
-      return session;
+        if (user) {
+          token.user = { ...user, provider: account.provider };
+        }
+        return token;
+      },
     },
-    async jwt({ token, user, account }) {
-      if (user) {
-        token.user = { ...user, provider: account.provider };
-      }
-      return token;
+    pages: {
+      signIn: '/account',
     },
-  },
-  pages: {
-    signIn: '/account',
-  },
-  secret: process.env.NEXT_AUTH_TOKEN_SECRET,
-  debug: true,
-});
+    secret: process.env.NEXT_AUTH_TOKEN_SECRET,
+    debug: true,
+  });
+}
