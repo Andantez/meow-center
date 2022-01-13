6449cf4d9eacd6335ff0b006758898a027195a54
Andantez <kaloyanatanasov83@gmail.com>
2022-01-13 20:11:18 +0000

    Change: updatae user name in db to check if the function runs in production.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -72,17 +72,20 @@ export default async function auth(req, res) {
           console.log(req.url === '/api/auth/session?update');
           const client = await clientPromise;
           const db = await client.db();
-          const u_ObjectId = new ObjectId(token.user.id);
-          const existingUser = await db
-            .collection('users')
-            .findOne({ _id: u_ObjectId });
-          const updatedUser = {
-            name: existingUser.name,
-            email: existingUser.email,
-            id: existingUser._id.toString(),
-            provider: token.user.provider,
-          };
-          token.user = updatedUser;
+          // const u_ObjectId = new ObjectId(token.user.id);
+          // const existingUser = await db
+          //   .collection('users')
+          //   .findOne({ _id: u_ObjectId });
+          // const updatedUser = {
+          //   name: existingUser.name,
+          //   email: existingUser.email,
+          //   id: existingUser._id.toString(),
+          //   provider: token.user.provider,
+          // };
+          // token.user = updatedUser;
+          const test = await db.collection("users").updateOne({email: "ades@gmail.com",}, {
+            $set: {name: "Testing Name"}
+          })
         } else {
           user && (token.user = { ...user, provider: account.provider });
         }
